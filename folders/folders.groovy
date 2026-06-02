d9934263ebdddd4aa82a65a743f4b1ed67be060a
Craig Baugh <craig.m.baugh@gmail.com>
2026-06-02 09:02:03 +0100

    add folders for Grafana and Alertmanager pipelines

--- folders/folders.groovy
+++ folders/folders.groovy
@@ -3,3 +3,11 @@ folder('prometheus') {
     displayName('Prometheus')
     description('Main folder for Prometheus pipelines')
 }
+folder('grafana') {
+    displayName('Grafana')
+    description('Main folder for Grafana pipelines')
+}
+folder('alertmanager') {
+    displayName('Alertmanager')
+    description('Main folder for Alertmanager pipelines')
+}
