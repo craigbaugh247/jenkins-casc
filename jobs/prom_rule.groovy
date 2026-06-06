76409e2632bed244a32b03274de0ec76065da5a0
Craig Baugh <craig.m.baugh@gmail.com>
2026-06-06 20:49:23 +0100

    fix: update Prometheus rule validation command to use promtool directly for rule checks

--- jobs/prom_rule.groovy
+++ jobs/prom_rule.groovy
@@ -32,7 +32,7 @@ pipelineJob('prometheus-rules-pipeline-job') {
                                 echo 'Validating Prometheus rule files using promtool...'
                                 
                                 // Runs a temporary promtool container to check all yaml files in the rules directory
-                                sh 'docker run --rm --entrypoint /bin/sh -v $(pwd)/rules:/tmp/rules prom/prometheus:latest -c "/bin/promtool check rules /tmp/rules/*_rules.yml"'
+                                sh 'docker run --rm --entrypoint /bin/promtool -v $(pwd)/rules:/tmp/rules prom/prometheus:latest check rules /tmp/rules/'
                             }
                         }
 
