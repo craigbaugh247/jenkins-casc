7d89a10d85b0229bc9f083e3cab13bfffe982057
Craig Baugh <craig.m.baugh@gmail.com>
2026-06-09 11:38:12 +0100

    fix: add missing tools declaration for Go in Grafana dashboard pipeline

--- jobs/grafana_dash.groovy
+++ jobs/grafana_dash.groovy
@@ -6,6 +6,7 @@ pipelineJob('grafana-dashboards-pipeline-job') {
             script('''
                 pipeline {
                     agent any
+                    tools { go '1.26.0' }
 
                     environment {
                         // Define your Grafana server instance details globally
